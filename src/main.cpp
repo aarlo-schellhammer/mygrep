3ef7bbcd15b9b0c94b9adfb1df51e48caf6e97bc
Aarlo Schellhammer <aarlosch@gmail.com>
2026-02-26 16:39:38 +0200

    Increment 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,42 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
-int main()
-{
-    string source;
-    string search;
+// Prototypes
+int Interactive();
+int FileSearch(string search, string filename);
 
-    cout << "Give string from which to search for: ";
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        Interactive();
+    }
+    else if (argc == 3)
+    {
+        string search = argv[1];
+        string filename = argv[2];
+        FileSearch(search, filename);
+    }
+    else
+    {
+        cout << "Wrong usage, try: \n"
+             << "mygrep.exe\n"
+             << "mygrep.exe <search> <filename>";
+    }
+    return 0;
+}
+
+int Interactive()
+{
+    string source, search;
+
+    cout << "Give a string from which to search for: ";
     getline(cin, source);
 
-    cout << "Give search string: ";
+    cout << "Give a search string: ";
     getline(cin, search);
 
     auto pos = source.find(search);
@@ -24,6 +49,27 @@ int main()
     {
         cout << "\"" << search << "\"" << " was NOT found in " << "\"" << source << "\".";
     }
-
     return 0;
+}
+
+int FileSearch(string search, string filename)
+{
+    ifstream file(filename);
+    if (!file)
+    {
+        cout << "Could not open the file\n";
+        return 1;
+    }
+    else
+    {
+        string line;
+        while (getline(file, line))
+        {
+            if (line.find(search) != string::npos)
+            {
+                cout << line << endl;
+            }
+        }
+        return 0;
+    }
 }
