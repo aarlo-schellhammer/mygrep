6024c345aab96a5eea9b77c69e6fc8ed0378e9c8
Aarlo Schellhammer <aarlosch@gmail.com>
2026-02-26 17:30:33 +0200

    Increment 3
    Added an optional -o flag followed by option letters: l (line numbers) and/or o (occurrence count)

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,49 @@
 
 using namespace std;
 
+// Struct to hold search options
+struct SearchOptions
+{
+    bool showLineNumbers;
+    bool showOccurrenceCount;
+};
+
 // Prototypes
 int Interactive();
-int FileSearch(string search, string filename);
+int FileSearch(const string &search, const string &filename, SearchOptions options);
+SearchOptions ParseOptions(const string &options);
 
 int main(int argc, char **argv)
 {
     if (argc == 1)
     {
-        Interactive();
+        return Interactive();
     }
     else if (argc == 3)
     {
         string search = argv[1];
         string filename = argv[2];
-        FileSearch(search, filename);
+
+        SearchOptions opts{false, false}; // no options
+        return FileSearch(search, filename, opts);
+    }
+    else if (argc == 4)
+    {
+        string optStr = argv[1];
+        string search = argv[2];
+        string filename = argv[3];
+
+        SearchOptions opts = ParseOptions(optStr);
+        return FileSearch(search, filename, opts);
     }
     else
     {
-        cout << "Wrong usage, try: \n"
+        cout << "Wrong usage, try:\n"
              << "mygrep.exe\n"
-             << "mygrep.exe <search> <filename>";
+             << "mygrep.exe <search> <filename>\n"
+             << "mygrep.exe -o[lo] <search> <filename>\n";
+        return 1;
     }
-    return 0;
 }
 
 int Interactive()
@@ -36,40 +56,75 @@ int Interactive()
     cout << "Give a string from which to search for: ";
     getline(cin, source);
 
-    cout << "Give a search string: ";
+    cout << "Give search string: ";
     getline(cin, search);
 
     auto pos = source.find(search);
 
     if (pos != string::npos)
     {
-        cout << "\"" << search << "\"" << " was found in " << "\"" << source << "\"" << " in position " << pos;
+        cout << "\"" << search << "\" found in \"" << source
+             << "\" in position " << pos << endl;
     }
     else
     {
-        cout << "\"" << search << "\"" << " was NOT found in " << "\"" << source << "\".";
+        cout << "\"" << search << "\" not found in \"" << source << "\"" << endl;
     }
     return 0;
 }
 
-int FileSearch(string search, string filename)
+int FileSearch(const string &search, const string &filename, SearchOptions options)
 {
     ifstream file(filename);
     if (!file)
     {
-        cout << "Could not open the file\n";
+        cout << "Could not open the file \"" << filename << "\"\n";
         return 1;
     }
-    else
+
+    string line;
+    int lineNumber = 0;
+    int count = 0;
+
+    while (getline(file, line))
     {
-        string line;
-        while (getline(file, line))
+        lineNumber++;
+
+        if (line.find(search) != string::npos)
         {
-            if (line.find(search) != string::npos)
+            count++;
+
+            if (options.showLineNumbers)
             {
-                cout << line << endl;
+                cout << lineNumber << ": ";
             }
+
+            cout << line << endl;
         }
-        return 0;
     }
+
+    if (options.showOccurrenceCount)
+    {
+        cout << "Occurrences of lines containing \"" << search << "\": " << count << endl;
+    }
+
+    return 0;
+}
+
+SearchOptions ParseOptions(const string &options)
+{
+    SearchOptions opts{false, false};
+
+    for (auto i = 2; i < options.length(); i++)
+    {
+        if (options[i] == 'l')
+        {
+            opts.showLineNumbers = true;
+        }
+        else if (options[i] == 'o')
+        {
+            opts.showOccurrenceCount = true;
+        }
+    }
+    return opts;
 }
