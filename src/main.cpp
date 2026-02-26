90380b2b934c6438685e4c6f92d12f00ba63726a
Aarlo Schellhammer <aarlosch@gmail.com>
2026-02-26 19:23:30 +0200

    Comments + no matches fix
    Commented code and added print if the searched word is not found in source file

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,13 @@
 
 using namespace std;
 
-// Struct to hold search options
+// Struct for search options
 struct SearchOptions
 {
-    bool showLineNumbers;
-    bool showOccurrenceCount;
-    bool caseInsensitive;
-    bool reverseSearch;
+    bool showLineNumbers;     // 'l' flag
+    bool showOccurrenceCount; // 'o' flag
+    bool caseInsensitive;     // 'i' flag
+    bool reverseSearch;       // 'r' flag
 };
 
 // Prototypes
@@ -26,10 +26,12 @@ int main(int argc, char **argv)
 {
     try
     {
+        // No arguments = interactive mode
         if (argc == 1)
         {
             return Interactive();
         }
+        // 2 arguments = simple file search
         else if (argc == 3)
         {
             string search = argv[1];
@@ -38,37 +40,42 @@ int main(int argc, char **argv)
             SearchOptions opts{false, false, false, false}; // no options
             return FileSearch(search, filename, opts);
         }
+        // 3 arguments = options + file search
         else if (argc == 4)
         {
             string optStr = argv[1];
             string search = argv[2];
             string filename = argv[3];
 
-            // Expect "-o<flags>" (allow "-o" with no flags too)
+            // Validate that options starts with "-o"
             if (optStr.size() < 2 || optStr[0] != '-' || optStr[1] != 'o')
             {
                 PrintUsage();
                 return 1;
             }
 
+            // Parse flags to SearchOption Struct
             SearchOptions opts = ParseOptions(optStr);
             return FileSearch(search, filename, opts);
         }
         else
         {
+            // If invalid number of arguments
             PrintUsage();
             return 1;
         }
     }
     catch (const exception &e)
     {
-        // Match typical assignment-style exception output more closely
+        // Exception handling
         cout << "Exception Nr. -1" << endl;
         cout << e.what() << endl;
         return -1;
     }
+    return 0;
 }
 
+// Interactive mode (user inputs source and search manually)
 int Interactive()
 {
     string source, search;
@@ -81,6 +88,7 @@ int Interactive()
 
     auto pos = source.find(search);
 
+    // Finds position inside source
     if (pos != string::npos)
     {
         cout << "\"" << search << "\" found in \"" << source
@@ -88,22 +96,24 @@ int Interactive()
     }
     else
     {
-        // Spec expects NOT uppercase
         cout << "\"" << search << "\" NOT found in \"" << source << "\"" << endl;
     }
     return 0;
 }
 
+// Performs file search based on given flags/options
 int FileSearch(const string &search, const string &filename, SearchOptions options)
 {
     ifstream file(filename);
+
+    // Throw exception if unable to open file
     if (!file)
     {
-        // Add period to match common exact-output specs
         throw runtime_error("Could not open the file \"" + filename + "\".");
     }
 
     string searchToCheck = search;
+    // Convert search to lowercase if 'i' flag is used
     if (options.caseInsensitive)
     {
         transform(searchToCheck.begin(), searchToCheck.end(), searchToCheck.begin(),
@@ -115,11 +125,14 @@ int FileSearch(const string &search, const string &filename, SearchOptions optio
     int lineNumber = 0;
     int count = 0;
 
+    // Read file line by line
     while (getline(file, line))
     {
         lineNumber++;
 
         string lineToCheck = line;
+
+        // Convert line to lowercase if 'i' flag is used
         if (options.caseInsensitive)
         {
             transform(lineToCheck.begin(), lineToCheck.end(), lineToCheck.begin(),
@@ -127,26 +140,40 @@ int FileSearch(const string &search, const string &filename, SearchOptions optio
                       { return static_cast<char>(tolower(c)); });
         }
 
+        // Check if line contains search string
         bool match = (lineToCheck.find(searchToCheck) != string::npos);
+
+        // Reverse result if 'r' flag is used
         if (options.reverseSearch)
         {
             match = !match;
         }
 
+        // If line matches print it
         if (match)
         {
             count++;
 
+            // Print line numbers if 'l' flag is used
             if (options.showLineNumbers)
             {
                 cout << lineNumber << ": ";
             }
-            cout << line << endl;
+            // If no matches found for search string
+            if (count == 0)
+            {
+                cout << "No matches found for \"" << search << "\"" << endl;
+            }
+            else
+            {
+                cout << line << endl;
+            }
         }
     }
-
+    // Print total occurrence count if 'o' flag is used
     if (options.showOccurrenceCount)
     {
+        // If reverse flag used change print to "NOT containing"
         if (options.reverseSearch)
         {
             cout << "Occurrences of lines NOT containing \"" << search << "\": " << count << endl;
@@ -160,12 +187,14 @@ int FileSearch(const string &search, const string &filename, SearchOptions optio
     return 0;
 }
 
+// Parses option string into SearchOptions struct
 SearchOptions ParseOptions(const string &options)
 {
+    // All options false by default
     SearchOptions opts{false, false, false, false};
 
-    // options format is "-o<flags>", flags start at index 2
-    for (auto i = 2; i < options.length(); i++)
+    // Starts from index 2 to skip "-o"
+    for (size_t i = 2; i < options.length(); i++)
     {
         switch (options[i])
         {
@@ -182,12 +211,14 @@ SearchOptions ParseOptions(const string &options)
             opts.reverseSearch = true;
             break;
         default:
+            // If unknown flag throw exception
             throw runtime_error(string("Option not recognized: '") + options[i] + "'");
         }
     }
     return opts;
 }
 
+// Prints instructions if user input is invalid
 void PrintUsage()
 {
     cout << "Wrong usage, try:\n"
